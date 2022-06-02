4da331203faa8d108965f2c5f3137475a2806efc
kyo144 <kwsmcy@gmail.com>
2022-06-02 18:30:49 +0800

    backend: change util function abort_msg's name

--- backend/utils/abort_msg.py
+++ backend/utils/abort_msg.py
@@ -4,7 +4,7 @@ import sys
 # 自訂的 API 錯誤訊息
 
 
-def abort_msg(e, custom_msg=''):
+def generate_abort_msg(e, custom_msg=''):
   error_class = e.__class__.__name__
   detail = e.args[0]
   cl, exc, tb = sys.exc_info()  # 錯誤完整資訊 Call Stack
@@ -12,7 +12,6 @@ def abort_msg(e, custom_msg=''):
   file_name = last_callstack[0]
   line_num = last_callstack[1]
   func_name = last_callstack[2]
-  print(
-      f'Exception raise in file: {file_name}, line {line_num}, in {func_name}: [{error_class}] {detail} {custom_msg}')
+  print(f'Exception raise in file: {file_name}, line {line_num}, in {func_name}: [{error_class}] {detail} {custom_msg}')
   msg_for_client = custom_msg if custom_msg != '' else detail
   return f'{msg_for_client}'
