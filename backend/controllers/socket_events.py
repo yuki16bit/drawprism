b100a580e2e3a3651441b1174a899a2b9c0313ba
kyo144 <kwsmcy@gmail.com>
2022-06-02 18:34:22 +0800

    backend: move logic of socket events to controllers folder

--- backend/controllers/socket_events.py
+++ backend/controllers/socket_events.py
@@ -1,22 +1,30 @@
-import shortuuid
 from wsgi import socket_io
 from flask_socketio import send, emit
-from flask import session
+from models.user import generate_jwt_token, verify_jwt_token, generate_anonymous_user
 
-@socket_io.on('connect')
-def socket_connect():
-  new_member_name = 'Guest-' + shortuuid.uuid()[:4]
-  session.permanent = True
-  session['member_name'] = new_member_name
+@socket_io.on('authentication')
+def authentication(jwt_cookie):
+  # 檢查 User 有無攜帶 JWT
+  user=None
+  if jwt_cookie:
+    # 若有，自動驗證 JWT
+    user=verify_jwt_token(jwt_cookie)
+    emit('anonymous-signin', user)
+    if not user:
+      # 若 JWT 驗證失敗或過期，補登記成新匿名遊客
+      user=generate_anonymous_user()
+      jwt = generate_jwt_token(user['uuid'], user['name'])
+      emit('anonymous-signup', jwt)
+  else:
+    # 若無，登記成新匿名遊客
+    user = generate_anonymous_user()
+    jwt = generate_jwt_token(user['uuid'], user['name'])
+    emit('anonymous-signup', jwt)
 
-@socket_io.on('message')
-def handleMessage(msg):
-  send(msg, broadcast=True)
-
-@socket_io.on('member')
-def socket_member():
-  send(session['member_name'], broadcast=True)
+@socket_io.on('chatting')
+def chatting(chatting_text):
+  send(chatting_text, broadcast=True)
 
 @socket_io.on('drawing')
-def handleDraw(data):
+def drawing(data):
   emit('drawing', data, broadcast=True)
