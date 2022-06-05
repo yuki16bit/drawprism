c896626b532d4e9800d43187e3e958f91a0c4d33
kyo144 <kwsmcy@gmail.com>
2022-06-05 16:48:19 +0800

    backend: install and remove some packages, rename duplicate func name

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,16 +1,15 @@
-import eventlet
-eventlet.monkey_patch(socket=True)
+from gevent import monkey
+monkey.patch_all()
 
-from models.db_rds import init_connection_pool, get_connection
 from controllers.user import bp_c_user
-from flask import Flask, current_app
+from controllers.setting import bp_c_setting
+from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 import os
 
-app = Flask(__name__, static_folder='../front-end/build', static_url_path='/')
-with app.app_context():
-  current_app.connection_pool = init_connection_pool()
-  current_app.get_connection = get_connection
+app = Flask(__name__, static_folder='../front-end/build', static_url_path='/') 
+CORS(app, origins=[os.getenv('DEV_ORIGIN'), os.getenv('PROD_ORIGIN')]) 
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['JWT_ALG'] = os.getenv('JWT_ALG')
@@ -19,15 +18,16 @@ app.config['JWT_ALG'] = os.getenv('JWT_ALG')
 def index():
   return app.send_static_file('index.html')
 
-@app.errorhandler(404)
-def not_found(e):
+@app.errorhandler(400)
+def bad_request(e):
   return app.send_static_file('index.html')
 
 @app.errorhandler(500)
-def not_found(e):
+def internal_error(e):
   return app.send_static_file('index.html')
 
 app.register_blueprint(bp_c_user, url_prefix='/api')
+app.register_blueprint(bp_c_setting, url_prefix='/api')
 
-socket_io = SocketIO(app, cors_allowed_origins='*', async_mode='eventlet', logger=True)
+socket_io = SocketIO(app, cors_allowed_origins=[os.getenv('DEV_ORIGIN'), os.getenv('PROD_ORIGIN')], async_mode='gevent', logger=True)
 from controllers import socket_events
