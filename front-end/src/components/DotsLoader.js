ce4f3db6d48a1c63a3be1a2d868636460d71940b
kyo144 <kwsmcy@gmail.com>
2022-06-06 19:54:45 +0800

    front-end: improve DotsLoader's behavior

--- front-end/src/components/DotsLoader.js
+++ front-end/src/components/DotsLoader.js
@@ -1,15 +1,20 @@
 const DotsLoader = ({ wrapClassName, dotSize }) => {
   return (
     <div className={`${wrapClassName}`}>
-      <span
-        className={`block ${`w-${dotSize} h-${dotSize}`} animate-dot-flashing rounded-full`}
-      ></span>
-      <span
-        className={`block ${`w-${dotSize} h-${dotSize}`} animate-dot-flashing rounded-full animation-delay-100`}
-      ></span>
-      <span
-        className={`block ${`w-${dotSize} h-${dotSize}`} animate-dot-flashing rounded-full animation-delay-200`}
-      ></span>
+      {dotSize === '2' && (
+        <>
+          <span className='block h-2 w-2 animate-dot-flashing rounded-full'></span>
+          <span className='block h-2 w-2 animate-dot-flashing rounded-full animation-delay-100'></span>
+          <span className='block h-2 w-2 animate-dot-flashing rounded-full animation-delay-200'></span>
+        </>
+      )}
+      {dotSize === '4' && (
+        <>
+          <span className='block h-4 w-4 animate-dot-flashing rounded-full'></span>
+          <span className='block h-4 w-4 animate-dot-flashing rounded-full animation-delay-100'></span>
+          <span className='block h-4 w-4 animate-dot-flashing rounded-full animation-delay-200'></span>
+        </>
+      )}
     </div>
   );
 };
