d966db2aa43854381ded2724299751743e528bb5
kyo144 <kwsmcy@gmail.com>
2022-06-08 10:24:42 +0800

    backend: improve update room logic and auto formatting code

--- backend/models/setting.py
+++ backend/models/setting.py
@@ -2,49 +2,51 @@ from datetime import datetime
 from models.db_mongo_atlas import db
 import shortuuid
 
+
 def generate_room(owner_uuid):
   room_uuid = shortuuid.uuid()[:9]
   room = {
-    'owner_uuid': owner_uuid,
-    'room_uuid': room_uuid,
-    'room_name': 'room @ ' + room_uuid,
-    'room_description': '',
-    'thumbnail': '',
-    'canvas_size': 'square',
-    'is_active': False,
-    'create_on': datetime.utcnow(),
-    'last_activity': ''
+      'owner_uuid': owner_uuid,
+      'room_uuid': room_uuid,
+      'room_name': 'room @ ' + room_uuid,
+      'room_description': '',
+      'thumbnail': '',
+      'canvas_size': 'square',
+      'is_active': False,
+      'create_on': datetime.utcnow(),
+      'last_activity': ''
   }
   return room
 
+
 def add_room(room):
   db.rooms.insert_one(room)
-  db.room_participates.insert_one({'room_uuid': room['room_uuid'], 'participates': []})
-  db.room_screen_shots.insert_one({'room_uuid': room['room_uuid'], 'screen_shots': []})
-  db.room_chat_logs.insert_one({'room_uuid': room['room_uuid'], 'chat_logs': []})
+  db.room_participates.insert_one(
+      {'room_uuid': room['room_uuid'], 'participates': []})
+  db.room_screen_shots.insert_one(
+      {'room_uuid': room['room_uuid'], 'screen_shots': []})
+  db.room_chat_logs.insert_one(
+      {'room_uuid': room['room_uuid'], 'chat_logs': []})
+
 
 def query_room_uuid(room_uuid):
-  return db.rooms.find_one({ 'room_uuid': room_uuid }, {'_id': False})
+  return db.rooms.find_one({'room_uuid': room_uuid}, {'_id': False})
 
-def query_participates(room_uuid):
-  return db.room_participates.find_one({ 'room_uuid': room_uuid }, {'_id': False, 'room_uuid': False})
 
 def query_screen_shots(room_uuid):
-  return db.rooms.find_one({ 'room_uuid': room_uuid }, {'_id': False})
+  return db.rooms.find_one({'room_uuid': room_uuid}, {'_id': False})
+
 
 def query_chat_logs(room_uuid):
-  return db.room_chat_logs.find_one({ 'room_uuid': room_uuid }, {'_id': False, 'room_uuid': False})
+  return db.room_chat_logs.find_one({'room_uuid': room_uuid}, {'_id': False, 'room_uuid': False})
 
-def update_room(room_uuid, room_name, room_description, canvas_size):
-  room=query_room_uuid(room_uuid)
+
+def update_room(updates):
   db.rooms.update_one(
-    { 'room_uuid': room_uuid },
-    {'$set': {
-      'room_name': room_name if room['room_name'] != room_name else room['room_name'],
-      'room_description': room_description if room['room_description'] != room_description else room['room_description'],
-      'canvas_size': canvas_size if room['canvas_size'] != canvas_size else room['canvas_size']
-    }}
+      {'room_uuid': updates['room_uuid']},
+      {'$set': updates}
   )
+
 
 def remove_room(room_uuid):
   db.rooms.delete_one({'room_uuid': room_uuid})
