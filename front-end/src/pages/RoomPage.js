f2705086aea5774d8741dcbddf58e24a5329f24d
kyo144 <kwsmcy@gmail.com>
2022-06-09 18:10:39 +0800

    front-end: restructure room page

--- front-end/src/pages/RoomPage.js
+++ front-end/src/pages/RoomPage.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { socketIoActions } from '../features/socketIoSlice';
@@ -6,89 +6,74 @@ import { useUpdateParticipateMutation } from '../features/apiSlice';
 import Canvas from '../components/Canvas';
 import ToolBox from '../components/ToolBox';
 import ChatBox from '../components/ChatBox';
-import Spacer from '../components/Spacer';
 
 const RoomPage = () => {
   const dispatch = useDispatch();
+
   const { state } = useLocation();
   let navigate = useNavigate();
-  const isConnected = useSelector((state) => state.socketIo.isConnected);
+
   const [updateParticipate] = useUpdateParticipateMutation();
-  const currentParticipate = () => {
+
+  const currentParticipate = useCallback(() => {
     return {
-      userUuid: state.userUuid,
-      userName: state.userName,
-      role: state.role,
-      mode: state.mode,
+      userUuid: state?.userUuid,
+      userName: state?.userName,
+      role: state?.role,
+      mode: state?.mode,
     };
-  };
+  }, [state?.mode, state?.role, state?.userName, state?.userUuid]);
 
   useEffect(() => {
-    if (
-      !state ||
-      !state?.roomUuid ||
-      !state?.mode ||
-      !state?.role ||
-      !state?.userName ||
-      !state?.userUuid ||
-      !state?.canvasSize
-    ) {
+    if (state === null) {
       navigate('/', { replace: true });
-    }
-  }, [navigate, state]);
-
-  useEffect(() => {
-    dispatch(socketIoActions.startConnecting());
-
-    const leaveAndDisconnect = () => {
+    } else {
       dispatch(
-        socketIoActions.leaveRoom({
-          roomUuid: state.roomUuid,
+        socketIoActions.joinRoom({
+          roomUuid: state?.roomUuid,
           participate: currentParticipate(),
         })
       );
       updateParticipate({
-        roomUuid: state.roomUuid,
+        roomUuid: state?.roomUuid,
+        action: 'join',
+        participate: currentParticipate(),
+      });
+    }
+
+    const leave = () => {
+      dispatch(
+        socketIoActions.leaveRoom({
+          roomUuid: state?.roomUuid,
+          participate: currentParticipate(),
+        })
+      );
+      updateParticipate({
+        roomUuid: state?.roomUuid,
         action: 'leave',
         participate: currentParticipate(),
       });
     };
 
-    window.addEventListener('beforeunload', leaveAndDisconnect);
+    window.addEventListener('beforeunload', leave);
 
     return () => {
-      leaveAndDisconnect();
-      window.removeEventListener('beforeunload', leaveAndDisconnect);
+      if (state !== null) leave();
+      window.removeEventListener('beforeunload', leave);
     };
-  }, []);
-
-  useEffect(() => {
-    if (isConnected) {
-      dispatch(
-        socketIoActions.joinRoom({
-          roomUuid: state.roomUuid,
-          participate: currentParticipate(),
-        })
-      );
-      updateParticipate({
-        roomUuid: state.roomUuid,
-        action: 'join',
-        participate: currentParticipate(),
-      });
-    }
-    return;
-  }, [isConnected]);
+  }, [currentParticipate, dispatch, navigate, state, updateParticipate]);
 
   return (
-    <div className='absolute left-0 top-0 h-[10000px] max-h-[10000px] w-[10000px] max-w-[10000px] bg-neutral-300'>
-      <Spacer width='w-6' height='h-6' minWidth='min-w-6' minHeight='min-h-6' />
-      <ToolBox />
-      <Canvas
-        locationState={state}
-        width={state.canvasSize === 'square' ? 2000 : 2479}
-        height={state.canvasSize === 'square' ? 2000 : 1750}
-      />
-      <ChatBox locationState={state} />
+    <div className='h-[1920px] w-[3000px] bg-neutral-300'>
+      <>
+        <ToolBox />
+        <Canvas
+          locationState={state}
+          width={state?.canvasSize === 'square' ? 2000 : 2479}
+          height={state?.canvasSize === 'square' ? 2000 : 1750}
+        />
+        <ChatBox locationState={state} />
+      </>
     </div>
   );
 };
