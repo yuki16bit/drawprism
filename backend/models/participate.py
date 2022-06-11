a1b2babe1c38644553432bb7b100ac8ea2121f94
kyo144 <kwsmcy@gmail.com>
2022-06-11 15:34:41 +0800

    backend: build query participate by request sid function

--- backend/models/participate.py
+++ backend/models/participate.py
@@ -5,6 +5,17 @@ def query_all_participate(room_uuid):
   return db.room_participates.find_one({'room_uuid': room_uuid}, {'_id': False})
 
 
+def query_participate_request_sid(request_sid):
+  try:
+    cursor = db.room_participates.find({'participates': {'$elemMatch': {'request_sid': {'$eq': request_sid}}}})
+    for result in cursor:
+      for participate in result['participates']:
+        if participate['request_sid'] == request_sid:
+          return participate
+  except:
+    return None
+
+
 def join_participate(join_data):
   db.room_participates.update_one(
       {'room_uuid': join_data['room_uuid']},
