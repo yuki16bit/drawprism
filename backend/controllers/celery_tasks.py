316195d045c3bb0e7743bd9d5e77286052a51742
kyo144 <kwsmcy@gmail.com>
2022-06-13 12:46:36 +0800

    backend: implement draw log feature - 1

--- backend/controllers/celery_tasks.py
+++ backend/controllers/celery_tasks.py
@@ -2,8 +2,12 @@ from wsgi import celery_app
 from flask_socketio import SocketIO
 from models.participate import leave_participate
 from models.chat_log import add_chat_log
+from models.draw_log import add_draw_log
+import os
 
-local_socket_io = SocketIO(message_queue='redis://localhost:6379')
+redis_url = os.getenv('PROD_REDIS_CLOUD_URL') if os.getenv(
+    'FLASK_ENV') == 'production' else os.getenv('DEV_REDIS_CLOUD_URL')
+local_socket_io = SocketIO(message_queue=redis_url)
 
 
 @celery_app.task()
@@ -16,3 +20,8 @@ def pull_participate_after_disconnect(participate):
 @celery_app.task()
 def record_chat_log(room_uuid, chatting_data):
   add_chat_log(room_uuid, chatting_data)
+
+
+@celery_app.task()
+def record_draw_log(room_uuid, drawing_data):
+  add_draw_log(room_uuid, drawing_data)
---
+++ backend/controllers/draw_log.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, abort, jsonify, make_response
+from utils.abort_msg import generate_abort_msg
+from models.draw_log import query_previous_draw_log
+from flask_cors import CORS
+import os
+
+
+bp_c_draw_log = Blueprint('c_draw_log', __name__)
+CORS(bp_c_draw_log, origins=[os.getenv('DEV_ORIGIN'), os.getenv(
+    'PROD_ORIGIN'), os.getenv('PROD_ORIGIN_WWW')], supports_credentials=True)
+
+
+@bp_c_draw_log.route('/draw_log/<room_uuid>', methods=['GET'])
+def get_draw_log(room_uuid):
+  try:
+    if room_uuid:
+      previous_draw_log = query_previous_draw_log(room_uuid)
+      return make_response(jsonify(previous_draw_log))
+    else:
+      raise TypeError('Get all chat log failed: Must provide room uuid.')
+  except TypeError as e:
+    abort(400, description=generate_abort_msg(e))
+  except Exception as e:
+    abort(500, description=generate_abort_msg(e))
