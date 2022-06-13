42088feb47729784373c10e16da2238b0f17c6a4
kyo144 <kwsmcy@gmail.com>
2022-06-13 12:49:37 +0800

    backend: adjust CORS origin url

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -6,7 +6,9 @@ from controllers.user import bp_c_user
 from controllers.setting import bp_c_setting
 from controllers.participate import bp_c_participate
 from controllers.chat_log import bp_c_chat_log
-from flask import Flask
+from controllers.draw_log import bp_c_draw_log
+from utils.abort_msg import generate_abort_msg
+from flask import Flask, jsonify, make_response
 from flask_socketio import SocketIO
 from flask_cors import CORS
 import os
@@ -25,22 +27,26 @@ def index():
 
 @app.errorhandler(400)
 def bad_request(e):
-  return app.send_static_file('index.html')
+  return make_response(jsonify({'ok': False, 'message': generate_abort_msg(e)}))
 
 
 @app.errorhandler(500)
 def internal_error(e):
-  return app.send_static_file('index.html')
+  return make_response(jsonify({'ok': False, 'message': generate_abort_msg(e)}))
 
 
 app.register_blueprint(bp_c_user, url_prefix='/api')
 app.register_blueprint(bp_c_setting, url_prefix='/api')
 app.register_blueprint(bp_c_participate, url_prefix='/api')
 app.register_blueprint(bp_c_chat_log, url_prefix='/api')
+app.register_blueprint(bp_c_draw_log, url_prefix='/api')
+
+redis_url = os.getenv('PROD_REDIS_CLOUD_URL') if os.getenv(
+    'FLASK_ENV') == 'production' else os.getenv('DEV_REDIS_CLOUD_URL')
 
 socket_io = SocketIO(app, cors_allowed_origins=[os.getenv('DEV_ORIGIN'), os.getenv(
-    'PROD_ORIGIN')], async_mode='gevent', logger=True, message_queue=os.getenv('REDIS_CLOUD_URL'))
+    'PROD_ORIGIN'), os.getenv('PROD_ORIGIN_WWW')], async_mode='gevent', logger=True, message_queue=redis_url)
 
-celery_app = Celery(os.getenv('CELERY_APP_NAME'), broker=os.getenv('REDIS_CLOUD_URL'))
+celery_app = Celery(os.getenv('CELERY_APP_NAME'), broker=redis_url)
 
 from controllers import socket_events  # nopep8
