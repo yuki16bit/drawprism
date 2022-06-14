df36f6f9d29fc05c381f9e5b759142d0e4984d72
kyo144 <kwsmcy@gmail.com>
2022-06-14 17:33:43 +0800

    backend: implement get all draw log feature

--- backend/controllers/draw_log.py
+++ backend/controllers/draw_log.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, abort, jsonify, make_response
 from utils.abort_msg import generate_abort_msg
-from models.draw_log import query_previous_draw_log
+from models.draw_log import query_previous_draw_log, query_all_previous_draw_log
 from flask_cors import CORS
 import os
 
@@ -8,6 +8,15 @@ import os
 bp_c_draw_log = Blueprint('c_draw_log', __name__)
 CORS(bp_c_draw_log, origins=[os.getenv('DEV_ORIGIN'), os.getenv(
     'PROD_ORIGIN'), os.getenv('PROD_ORIGIN_WWW')], supports_credentials=True)
+
+
+@bp_c_draw_log.route('/draw_log', methods=['GET'])
+def get_all_draw_log():
+  try:
+    all_previous_draw_log = query_all_previous_draw_log()
+    return make_response(jsonify(all_previous_draw_log))
+  except Exception as e:
+    abort(500, description=generate_abort_msg(e))
 
 
 @bp_c_draw_log.route('/draw_log/<room_uuid>', methods=['GET'])
