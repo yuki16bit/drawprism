9b9772260fba50c0d5fb245998ca2bc8a7652645
kyo144 <kwsmcy@gmail.com>
2022-06-17 20:08:52 +0800

    backend: hotfix: fix draw log thumbnail problem

--- backend/models/draw_log.py
+++ backend/models/draw_log.py
@@ -1,4 +1,4 @@
-import collections
+from models.setting import query_all_active_room
 from models.db_mongo_atlas import db
 
 
@@ -14,9 +14,13 @@ def query_previous_draw_log(room_uuid):
 def query_all_previous_draw_log():
   try:
     all_draw_logs = []
-    collection = db.room_draw_logs.find({}, {'_id': False})
-    for doc in collection:
-      all_draw_logs.append(doc['draw_logs'])
+    current_all_active_room = query_all_active_room()
+    # collection = db.room_draw_logs.find({}, {'_id': False})
+    # for doc in collection:
+    # all_draw_logs.append(doc['draw_logs'])
+    for active_room in current_all_active_room:
+      draw_log = db.room_draw_logs.find_one({'room_uuid': active_room['room_uuid']}, {'_id': False})
+      all_draw_logs.append(draw_log['draw_logs'])
     return all_draw_logs
   except:
     return
